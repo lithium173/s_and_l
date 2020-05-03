99a0fc9e79a841135352396d646b8881a6cf0c96
Ali-128 <ali_hassani128@yahoo.com>
2020-05-03 21:19:24 +0430

    start constructor of player

--- objects.py
+++ objects.py
@@ -1,19 +1,33 @@
 class Player:
-	def __init__(self):
-		pass
-	def move(self):
-		pass
-class Table:
-	def __init__(self):
-		pass
-	def show(self):
-		pass
-class Bit:
-	def __init__(self):
-		pass
-class Dice:
-	def __init__(self):
-		pass
-	def randDice(self):
-		pass
+    def __init__(self, player_x, player_y, x_change, y_change):
+        self.player_x = player_x
+        self.player_y = player_y
+        self.x_change = x_change
+        self.y_change = y_change
 
+    def move(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class Table:
+    def __init__(self):
+        pass
+
+    def show(self):
+        pass
+
+
+class Bit:
+    def __init__(self):
+        pass
+
+
+class Dice:
+    def __init__(self):
+        pass
+
+    def randDice(self):
+        pass
